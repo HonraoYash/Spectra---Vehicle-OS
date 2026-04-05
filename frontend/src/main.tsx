984c9d146d1f9f87c1075860491ebb66eba55642
Yash Honrao <yashhonrao2024@gmail.com>
2026-04-05 15:33:37 -0500

    Enabled Web Analytics on Vercel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
@@ -5,6 +6,9 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <Analytics />
+    </>
   </StrictMode>,
 )
